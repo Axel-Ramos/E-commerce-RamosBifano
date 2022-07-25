0ac6d386b25e48e2ba1b7e99bff160632fa37413
Axel-Ramos <axelramoscir@gmail.com>
2022-07-25 17:12:09 -0300

    Agregando Fetch con error

---
+++ ecommerce-ramosbifano/src/Components/ItemListContainer/ItemDetailContainer/ItemDetail.jsx
@@ -0,0 +1,17 @@
+import Contador from "../ItemCount/ItemCount";
+
+function DetalleProducto(){
+    return(
+        <div className="detalleProducto">
+            <img src="#" alt="Imagen del Producto"/>
+            <div>
+                <h3 className="textoProductos">Nombre del Producto</h3>
+                <p className="textoProductos">Caracteristicas del Producto</p>
+                <p className="textoProductos">Precio del Producto</p>
+                <Contador/>
+                <button>Agregar al Carrito</button>
+            </div>
+        </div>
+    )
+}
+export default DetalleProducto;
--- ecommerce-ramosbifano/src/Components/ItemListContainer/ItemListContainer.jsx
+++ ecommerce-ramosbifano/src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,22 +1,20 @@
 import { useEffect, useState } from "react";
-import Contador from "./ItemCount/ItemCount";
-import { getFetch } from "../../helpers/productos";
+import ItemDetail from "./ItemDetailContainer/ItemDetail"
 import "./ItemListContainer.css";
 
 function ItemListContainer(){
     const [productos, setProductos]=useState([])
     const [carga, setCarga]=useState(true)
-
     useEffect(()=>{
-        getFetch()
+        fetch("../../Helpers/productos.json")
+        .then(respuesta=>respuesta.json())
         .then(respuesta=>setProductos(respuesta))
         .catch(err=>console.log(err))
         .finally(()=>setCarga(false))
     },[])
-
     console.log(productos)
     return(
-        <div>
+        <div className="itemContainer">
                 {carga ? <h1>Cargando...</h1>
                 :
                 productos?.map(producto=>
@@ -24,19 +22,10 @@ function ItemListContainer(){
                     <div className="products">
                         <h2>{producto.nombre}</h2>
                         <img src={producto.imagen} alt="Imagen Producto" />
-                        <button>Ver Detalles</button>
+                        <button className="botonDetalles">Ver Detalles</button>
                     </div>
-                </div>)
-                }
-
-            <div className="detalleProducto">
-                <h3>Nombre del Producto</h3>
-                <img src="#" alt="Imagen del Producto"/>
-                <p>Caracteristicas del Producto</p>
-                <p>Precio del Producto</p>
-                <Contador/>
-                <button>Agregar al Carrito</button>
-            </div>
+                </div>)}
+                <ItemDetail/>
         </div>
     )
 }
